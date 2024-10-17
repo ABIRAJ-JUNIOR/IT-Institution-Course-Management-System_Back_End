004055f5a997f65455aac592f4974699fd365242
Abiraj Vijayanathan <168581337+ABIRAJ-JUNIOR@users.noreply.github.com>
2024-10-17 16:39:19 +0530

    Add project files.

---
+++ IT-Institution-Course-Management-System/Program.cs
@@ -0,0 +1,56 @@
+namespace IT_Institution_Course_Management_System
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.WebHost.UseWebRoot("wwwroot");
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            var connectionString = builder.Configuration.GetConnectionString("DBConnection");
+
+
+
+
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors();
+
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
