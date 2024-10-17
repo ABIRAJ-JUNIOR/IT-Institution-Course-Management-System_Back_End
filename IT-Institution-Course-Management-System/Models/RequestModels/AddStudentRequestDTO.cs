66c6404dc8217ecaceb9cd31e7c964fa77f71ee5
Abiraj Vijayanathan <168581337+ABIRAJ-JUNIOR@users.noreply.github.com>
2024-10-17 17:26:19 +0530

    Student Table Models Added

---
+++ IT-Institution-Course-Management-System/Models/RequestModels/AddStudentRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace IT_Institution_Course_Management_System.Models.RequestModels
+{
+    public class AddStudentRequestDTO
+    {
+        public string Nic { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public int RegistrationFee { get; set; }
+        public IFormFile? ImageFile { get; set; }
+    }
+}
