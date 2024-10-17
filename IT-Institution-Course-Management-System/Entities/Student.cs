a0b967dfe70dc87dfebd1f5c7a304b6891461a29
Abiraj Vijayanathan <168581337+ABIRAJ-JUNIOR@users.noreply.github.com>
2024-10-17 17:06:21 +0530

    Student Entity Added

---
+++ IT-Institution-Course-Management-System/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace IT_Institution_Course_Management_System.Entities
+{
+    public class Student
+    {
+        public string Nic { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public int RegistrationFee { get; set; }
+        public string? CourseEnrollId { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
